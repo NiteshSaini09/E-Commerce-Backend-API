d600906c7b026b84ac5cfba0d38869c7fdb96e2a
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-08-31 23:52:43 +0530

    Phase 2 complete

---
+++ src/middleware/admin.middleware.js
@@ -0,0 +1,14 @@
+import { UserModel } from "../models/user.model.js"
+import ApiError from "../utils/ApiError.js"
+ const verifyAdmin=async(req,res,next)=>{
+    try {
+        // const user=await UserModel.findById(req.user?._id)
+        if(req.user.role !=='admin'){
+            throw new ApiError(402,"Access denied: Only Admin can access this route")
+        }
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+export default verifyAdmin
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,8 +2,14 @@ import { Router } from "express";
 import * as userController from '../controllers/user.controller.js'
 import validate from "../middleware/validate.middleware.js";
 import { loginSchema, registerSchema } from "../validators/user.validator.js";
+import verifyJWT from "../middleware/auth.middleware.js";
+import  verifyAdmin  from "../middleware/admin.middleware.js";
 const router=Router()
 
 router.route('/register').post(validate(registerSchema),userController.register)
 router.route('/login').post(validate(loginSchema),userController.login)
+router.route('/profile').get(verifyJWT,userController.profile)
+router.route('/log-out').get(verifyJWT,userController.logOut)
+router.route('/refresh-access-token').get(userController.refreshAccessToken)
+router.route('/admin-pannel').get(verifyJWT,verifyAdmin,userController.adminPannel)
 export default router
