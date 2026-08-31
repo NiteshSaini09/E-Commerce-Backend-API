c59aea162cb21e0a76948928b02d5d102cbc6ae3
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-08-31 23:52:43 +0530

    Implement user profile retrieval, logout functionality, and admin verification middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken"
 import { UserModel } from "../models/user.model.js"
 import  ApiError  from "../utils/ApiError.js"
 import mongoose from "mongoose"
@@ -24,6 +25,7 @@ export const register=async(req,res,next)=>{
         next(error)
     }
 }
+//  -------------login--------------
 
 export const login=async (req,res,next)=>{
     try {
@@ -33,7 +35,6 @@ export const login=async (req,res,next)=>{
             throw new ApiError(404,"Invalid email or password")
         }
         const isPasswordCorrect=await user.isPasswordCorrect(password)
-        console.log(isPasswordCorrect)
         if(!isPasswordCorrect){
             throw new ApiError(400,"Invalid email or password")
         }
@@ -55,6 +56,64 @@ export const login=async (req,res,next)=>{
             accessToken
         })
 
+    } catch (error) {
+        next(error)
+    }
+}
+
+//------------profile--------------
+export const profile=async (req,res,next)=>{
+    try {
+        const user=await UserModel.findById(req.user?._id).select("-password -refreshToken")
+        res.status(200).json({
+            success:true,
+            message:"User Profile Fatched successfully",
+            user
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export const adminPannel=async (req,res,next)=>{
+    res.send("I am admin")
+}
+
+
+export const logOut=async (req,res,next)=>{
+    try {
+        const user=await UserModel.findById(req.user?._id)
+        user.refreshToken=undefined
+        await user.save()
+        res.status(200).clearCookie("refreshToken").clearCookie("accessToken").json({
+            success:true,
+            message:"User loged out successfully"
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const refreshAccessToken=async (req,res,next)=>{
+    try {
+        const token=req.cookies?.refreshToken
+        if(!token){
+            throw new ApiError(400,"can't get Refresh Token, Please login first")
+        }
+        const decodedToken= jwt.verify(token,process.env.REFRESH_TOKEN_SECRET)
+        const user=await UserModel.findById(decodedToken?._id)
+        if(!user || token!==user.refreshToken){
+            throw new ApiError(400,"Invalid Token")
+        }
+        const newAccessToken=await user.generateAccessToken()
+        const options={
+            httpOnly:true
+        }
+        res.status(200).cookie("accessToken",newAccessToken,options).json({
+            success:true,
+            message:"access token refreshed successfully"
+        })
     } catch (error) {
         next(error)
     }
--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,7 +3,8 @@ import ApiError from "../utils/ApiError.js";
 import { UserModel } from "../models/user.model.js";
 export const verifyJWT=async(req,res,next)=>{
     try {
-        const token=req.cookie?.accessToken || req?.header("Authorization").replace("Bearer ","")
+        const token=req.cookies?.accessToken 
+        // || req?.header("Authorization").replace("Bearer ","")
         if(!token){
             throw new ApiError(401,"Unauthosized request")
         }
