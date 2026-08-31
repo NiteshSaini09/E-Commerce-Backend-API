be2d6da8c440be8e3003bea35051e3b2c0544bb5
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-08-31 21:48:14 +0530

    Add JWT verification middleware for authentication

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import ApiError from "../utils/ApiError.js";
+import { UserModel } from "../models/user.model.js";
+export const verifyJWT=async(req,res,next)=>{
+    try {
+        const token=req.cookie?.accessToken || req?.header("Authorization").replace("Bearer ","")
+        if(!token){
+            throw new ApiError(401,"Unauthosized request")
+        }
+        const decodedToken= jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+        const user=await UserModel.findById(decodedToken?._id)
+        if(!user || !decodedToken){
+            throw new ApiError(400,'Invalid Token')
+        }
+        req.user=user
+        next()
+    } catch (error) {
+        next(error)
+    }
+
+}
+export default verifyJWT
