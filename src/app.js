2764f7256bf6c3ab2f0cbb634a5f11476c13e18b
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-04 00:23:51 +0530

    Add EJS view engine support and create home view; update app.js for rendering and include EJS dependency in package.json.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,35 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import { errorHandler } from './middleware/errorHandler.middleware.js'
-import userRoutes from './routes/user.routes.js'
-import productRoutes from './routes/product.routes.js'
-import categoryRoutes from './routes/category.routes.js'
-const app=express()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
-app.use(cookieParser())
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorHandler } from "./middleware/errorHandler.middleware.js";
+import userRoutes from "./routes/user.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+app.set('views', path.join(__dirname, 'views'))
 
-app.get('/',(req,res)=>{
-    res.send("e commerse app is running")
-})
-app.use('/api/v1/user',userRoutes)
-app.use('/api/v1/product',productRoutes)
-app.use('/api/v1/category',categoryRoutes)
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+//   console.log(path.__dirname);
+//   console.log(path)
+//   console.log(path.join(__dirname, 'views'))
+//   res.send("e commerse app is running");
+res.render("home")
+});
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
-app.use(errorHandler)
-export {app}
+app.use(errorHandler);
+export { app };
