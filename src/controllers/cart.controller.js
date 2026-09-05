646b73ddee79ba898a7ae8954f5f7064d189664d
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-06 00:39:55 +0530

    Refactor add function in cart controller; enhance error handling and structure for adding items to cart
    There was logic problems that will sort tomorrow

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,4 +1,38 @@
-export const add=async(req,res,next)=>{
-    console.log("I am Here")
+import ApiError from "../utils/ApiError.js";
+import { ProductModel } from "../models/product.model.js";
+import { CartModel } from "../models/cart.model.js";
+export const add = async (req, res, next) => {
+  try {
+    const data={
+        user:req.user?._id,
+        items:[]
+    }
+    const item={}
+    if (req.body == undefined) {
+      throw new ApiError(400, "Please enter Product Id and quantity");
+    }
+    const productId=req.body?.productId
+    const quantity=req.body?.quantity
+    const product=await ProductModel.findById(productId)
+    if(!product){
+        throw new ApiError(404,"Product not found for add to cart")
+    }
+    item.product=productId
+    if(quantity>0){
+        item.quantity=quantity
+    }
+    data.items.push(item)
+
+    const cart=await CartModel.create(data)
+    console.log("cart created")
+    // console.log(data)/
+
+    res.status(200).json({
+        success:true,
+        cart
+    })
     
-}
+  } catch (error) {
+    next(error);
+  }
+};
