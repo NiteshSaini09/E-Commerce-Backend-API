8bf44294f937bc9bace0e6b42db1e6503d6cc5bb
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-05 23:34:02 +0530

    Fix formatting and structure in cart model; ensure consistent spacing and line breaks

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,22 +1,29 @@
+// import { required } from "joi";
+import { required } from "joi";
 import mongoose from "mongoose";
-const cartSchema=new mongoose.Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    items:[
-        {
-            prpduct:{
-                type:mongoose.Schema.Types.ObjectId,
-                ref:"Product"
-            },
-            quantity:{
-                type:Number,
-                default:1
-            }
-        }
-    ]
+    items: [
+      {
+        prpduct: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          min:1,
+          required:true,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
 
-},{timestamps:true})
-
-export const CartModal=mongoose.model("Cart",cartSchema)
+export const CartModal = mongoose.model("Cart", cartSchema);
