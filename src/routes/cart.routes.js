4fbbbf910cbf2755e9e960fc83c9fe0fe63f6b38
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-07 12:19:48 +0530

    Fix clear cart route method; change from GET to DELETE for proper RESTful API design

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -10,6 +10,6 @@ router.route("/").post(verifyJWT,validate(addSchema),controller.add)
 router.route("/").get(verifyJWT,controller.getCart)
 router.route("/update-uantity").post(verifyJWT,controller.updateQantity)
 router.route("/remove").delete(verifyJWT,controller.removeProduct)
-router.route("/clear").get(verifyJWT,controller.clearCart)
+router.route("/clear").delete(verifyJWT,controller.clearCart)
 
 export default router
