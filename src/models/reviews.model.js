28e1fba5612a323f7eef7b9c4dab3096b6ff42aa
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-09 22:43:53 +0530

    Add review model; implement schema for user reviews with validation

---
+++ src/models/reviews.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+const reviewSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+        trim:true
+    },
+    product:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Product",
+        required:true,
+        trim:true
+    },
+    rating:{
+        type:Number,
+        required:true,
+        min:1,
+        max:5
+    },
+    comment:{
+        type:String,
+        required:true,
+        trim:true,
+        maxlength:500
+    }
+
+},{timestamps:true})
+
+reviewSchema.index({user:1,product:1},{unique:true});
+
+export const ReviewModel=mongoose.model("Review",reviewSchema)
